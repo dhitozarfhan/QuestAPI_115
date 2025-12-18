35dada3d999b03bb92a07b571cf7a8535372887f
dhitozarfhan <dhitozarfhan@gmail.com>
2025-12-18 20:24:08 +0700

    membuat peta navigasi

--- app/src/main/java/com/example/localapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localapi/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,54 @@
 package com.example.localapi.uicontroller
 
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.localapi.uicontroller.route.DestinasiEntry
+import com.example.localapi.uicontroller.route.DestinasiHome
+import com.example.localapi.view.EntrySiswaScreen
+import com.example.localapi.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                navigateToItemUpdate = { id ->
+                    // nanti dipakai untuk update
+                    // navController.navigate("${DestinasiDetail.route}/$id")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
