2e4864f48a62cadb46cfa7f389c9d45ca482cd98
dhitozarfhan <dhitozarfhan@gmail.com>
2025-12-18 20:23:55 +0700

    membuat navigasi data siswa app

--- app/src/main/java/com/example/localapi/MainActivity.kt
+++ app/src/main/java/com/example/localapi/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.localapi.ui.theme.LocalApiTheme
+import com.example.localapi.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocalApiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
